accb0ae6f2d578e45f95cc0a0921c38f50e0ccfb
lakshya2601 <lakshyaporwal2601@gmail.com>
2022-09-15 19:48:05 +0530

    netflix clone complete

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Row from "./Row";
+import requests from './request';
+import Banner from "./Banner";
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Nav/>
+      <Banner/>
+      <Row title= "NETFLIX ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow={true}
+      />
+      <Row title= "Trending now" fetchUrl={requests.fetchTrending}/>
+      <Row title= "Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title= "Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title= "Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title= "Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title= "Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title= "Documentries Movies" fetchUrl={requests.fetchDocumentaries}/>
+      
+    </div>
+  );
+}
+
+export default App;
